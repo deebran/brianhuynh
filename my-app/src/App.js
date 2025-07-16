48bc29ce491d7b213d548c4dd531fdb6715bcd09
Brian Huynh <bah36@calvin.edu>
2025-07-16 19:27:38 -0400

    base website

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,9 +1,12 @@
-import './App.css';
-import React from "react";
+import About from "./components/About";
+import Contact from "./components/Contact";
+import Projects from "./components/Projects";
+import Skills from "./components/Skills";
+import Navbar from "./components/Navbar";
 
 export default function App() {
   return(
-    <main>
+    <main className="text-gray-400 bg-gray-900 body-font">
       <Navbar />
       <About />
       <Projects />
